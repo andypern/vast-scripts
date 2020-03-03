dd50bc438cd9ca3360f43627b65594bfbda837b4
Andy Pernsteiner <andypern@gmail.com>
2020-03-03 12:54:18 -0800

    added rdma options, other stuff

--- vast-perf.sh
+++ vast-perf.sh
@@ -4,9 +4,9 @@
 # make sure FIO is installed, put the path in the FIO_BIN variable.
 # you don't _need_ clustershell/clush , but it makes it easier to run this on multiple hosts.
 # 1.  put this script on client-1
-# 2.  Run it like this 'bash /home/vastdata/vast-perf.sh 10.100.201.201 read_bw 120 8'  <--- this will ONLY run on client, and will read_bw test for 120 seconds, with 8 numjobs.
+# 2.  Run it like this 'bash /home/vastdata/vast-perf.sh 10.100.201.201 / read_bw 120 8 1'  <--- this will ONLY run on client, and will read_bw test for 120 seconds, with 8 numjobs.
 # 3.  Once you verify it works, copy to all cnodes: `clush -g clients -c /home/vastdata/vast-perf.sh`
-# 4.  Run on all nodes like this `clush -g clients 'bash /home/vastdata/vast-perf.sh 10.100.201.201 read_bw 120 8'`
+# 4.  Run on all nodes like this `clush -g clients 'bash /home/vastdata/vast-perf.sh 10.100.201.201 read_bw 120 8 1'`
 
 
 ###Notes
@@ -17,9 +17,14 @@
 
 #positional $ARGS:
 mVIP=$1  # the VMS-VIP of the vast cluster you are testing against.
-TEST=$2 # one of 'write_bw' , 'read_bw', 'write_iops' , 'read_iops'
-RUNTIME=$3 # runtime in seconds of the test.
-JOBS=$4 # how many threads per host. This will also result in N mountpoints per host (up to the total number of VIPs in the vast cluster.)
+NFSEXPORT=$2
+TEST=$3 # one of 'write_bw' , 'read_bw', 'write_iops' , 'read_iops'
+RUNTIME=$4 # runtime in seconds of the test.
+JOBS=$5 # how many threads per host. This will also result in N mountpoints per host (up to the total number of VIPs in the vast cluster.)
+
+REMOTE_PATH="fio" # change this to whatever you need it to be.
+
+NFS=rdma # or tcp.
 
 ###other variables you can set.
 #set this to 1 if you want to delete between runs.
@@ -31,6 +36,10 @@ DELETE_ALL=0
 MOUNT=/mnt/demo #where the mountpoints will get created.
 
 FIO_BIN=/usr/bin/fio
+
+#libaio most of the time..
+ioengine=libaio
+#ioengine=psync
 
 
 #pick the ID of the vip pool you want to use..most of the time it will be '1'
@@ -60,6 +69,18 @@ for i in $client_VIPs; do
   all_vips+=(${i})
 done
 
+# shuffle...but this may not behave like you want.
+
+#all_vips=( $(shuf -e "${all_vips[@]}") )
+
+#but: if you are using read_bw_reuse , then you definitely want to shuffle.
+
+if [[ ${TEST} == "read_bw_reuse" ]] ; then
+  all_vips=( $(shuf -e "${all_vips[@]}") )
+fi
+
+
+
 
 sudo umount -lf ${MOUNT}/*
 
@@ -79,8 +100,12 @@ MD_DIRS=()
 
 for i in ${needed_vips[@]}
         do sudo mkdir -p ${MOUNT}/${i}
-        sudo mount -vvv -t nfs -o tcp,rw,vers=3 ${i}:/ ${MOUNT}/${i}
-        export fio_dir=${MOUNT}/$i/fio/${node}/${i}
+        if [[ ${NFS} == "rdma" ]] ; then
+          sudo mount -vvv -t nfs -o proto=rdma,port=20049 ${i}:/${NFSEXPORT} ${MOUNT}/${i}
+        else
+          sudo mount -vvv -t nfs -o tcp,rw,vers=3 ${i}:/${NFSEXPORT} ${MOUNT}/${i}
+        fi
+        export fio_dir=${MOUNT}/$i/${REMOTE_PATH}/${node}/${i}
         DIRS+=${fio_dir}:
         sudo mkdir -p ${fio_dir}
         sudo chmod 777 ${fio_dir}
@@ -91,24 +116,31 @@ done
 echo ${DIRS}
 
 write_bw_test () {
-  ${FIO_BIN} --name=randrw --ioengine=libaio --refill_buffers --create_serialize=0 --randrepeat=0 --create_on_open=1 --fallocate=none --iodepth=4 --rw=randrw --bs=1mb --direct=1 --size=20g --numjobs=${JOBS} --rwmixread=0 --group_reporting --directory=${DIRS} --time_based=1 --runtime=${RUNTIME}
+  ${FIO_BIN} --name=randrw --ioengine=${ioengine} --refill_buffers --create_serialize=0 --randrepeat=0 --create_on_open=1 --fallocate=none --iodepth=4 --rw=randrw --bs=1mb --direct=1 --size=20g --numjobs=${JOBS} --rwmixread=0 --group_reporting --directory=${DIRS} --time_based=1 --runtime=${RUNTIME}
 
 }
 
 
 read_bw_test () {
-  ${FIO_BIN} --name=randrw --ioengine=libaio --refill_buffers --create_serialize=0 --randrepeat=0 --fallocate=none --iodepth=6 --rw=randrw --bs=1mb --direct=1 --size=20g --numjobs=${JOBS} --rwmixread=100 --group_reporting --directory=${DIRS} --time_based=1 --runtime=${RUNTIME}
+  ${FIO_BIN} --name=randrw --ioengine=${ioengine} --refill_buffers --create_serialize=0 --randrepeat=0 --fallocate=none --iodepth=6 --rw=randrw --bs=1mb --direct=1 --size=20g --numjobs=${JOBS} --rwmixread=100 --group_reporting --directory=${DIRS} --time_based=1 --runtime=${RUNTIME}
 }
 
 
 write_iops_test () {
-  ${FIO_BIN} --name=randrw --ioengine=libaio --refill_buffers --create_serialize=0 --randrepeat=0 --create_on_open=1 --fallocate=none --iodepth=4 --rw=randrw --bs=4kb --direct=1 --size=20g --numjobs=${JOBS} --rwmixread=0 --group_reporting --directory=${DIRS} --time_based=1 --runtime=${RUNTIME}
+  ${FIO_BIN} --name=randrw --ioengine=${ioengine} --refill_buffers --create_serialize=0 --randrepeat=0 --create_on_open=1 --fallocate=none --iodepth=4 --rw=randrw --bs=4kb --direct=1 --size=20g --numjobs=${JOBS} --rwmixread=0 --group_reporting --directory=${DIRS} --time_based=1 --runtime=${RUNTIME}
 
 }
 
 read_iops_test () {
-  ${FIO_BIN} --name=randrw --ioengine=libaio --refill_buffers --create_serialize=0 --randrepeat=0 --fallocate=none --iodepth=4 --rw=randrw --bs=4kb --direct=1 --size=20g --numjobs=${JOBS} --rwmixread=100 --group_reporting --directory=${DIRS} --time_based=1 --runtime=${RUNTIME}
+  ${FIO_BIN} --name=randrw --ioengine=${ioengine} --refill_buffers --create_serialize=0 --randrepeat=0 --fallocate=none --iodepth=4 --rw=randrw --bs=4kb --direct=1 --size=20g --numjobs=${JOBS} --rwmixread=100 --group_reporting --directory=${DIRS} --time_based=1 --runtime=${RUNTIME}
 }
+
+read_bw_reuse_test () {
+  rando_dir=${MOUNT}/${all_vips[0]}/${REMOTE_PATH}/${node}
+  echo ${rando_dir}
+  ${FIO_BIN} --name=randrw --ioengine=${ioengine} --iodepth=6 --rw=randrw --bs=1mb --direct=1 --numjobs=${JOBS} --rwmixread=100 --group_reporting --opendir=${rando_dir} --time_based=1 --runtime=${RUNTIME}
+}
+
 
 
 
@@ -121,6 +153,8 @@ elif [[ ${TEST} == "write_iops" ]] ; then
   write_iops_test
 elif [[ ${TEST} == "read_iops" ]] ; then
   read_iops_test
+elif [[ ${TEST} == "read_bw_reuse" ]] ; then
+  read_bw_reuse_test
 else
   echo "you didn't specify a valid test. unmounting and exiting"
 fi
